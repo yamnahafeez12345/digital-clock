39e04b783a6293eef3678381cc3d889a4a001a81
yamnahafeez12345 <128935056+yamnahafeez12345@users.noreply.github.com>
2023-09-28 19:07:14 +0500

    Add files via upload

---
+++ DIGITAL CLOCK/app.js
@@ -0,0 +1,19 @@
+const time = document.getElementById('time');
+const timeformat = document.getElementById('timeformat');
+document.addEventListener('DOMContentLoaded',()=>{
+setInterval(showTime,1000);
+});
+
+const showTime = () =>{
+    let date = new Date();
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let secs = date.getSeconds();
+    hr =hr<10 ? `0${hr}` : hr;
+    min =min<10 ? `0${min}` : min;
+    secs =secs<10 ? `0${secs}` : secs;
+
+    // console.log("hours" + hr + "mins" + min + "secs" + secs);
+    time.innerHTML = `${hr} : ${min} : ${secs}`;
+    timeformat.innerHTML = hr>12 ? "PM" : "AM";
+}
